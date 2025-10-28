2df527f672d4f573b6e82051a2c238a64c344648
Ben Meier <ben.meier@humanitec.com>
2025-10-28 13:37:33 +0000

    fix: for-each

--- modules/aws-infra/k8s.tf
+++ modules/aws-infra/k8s.tf
@@ -5,8 +5,9 @@ module "k8s-common" {
 }
 
 resource "platform-orchestrator_module" "k8s-cluster-credentials" {
+  for_each      = toset(var.runtime == "kubernetes" ? ["this"] : [])
   id            = "aws-k8s-cluster-credentials"
-  resource_type = module.k8s-common.cluster_credentials_resource_type
+  resource_type = module.k8s-common["this"].cluster_credentials_resource_type
   module_source = "inline"
   module_inputs = jsonencode({
     env_type_id = "$${context.env_type_id}"
@@ -37,6 +38,7 @@ EOT
 }
 
 resource "platform-orchestrator_module_rule" "cluster-creds" {
-  module_id   = platform-orchestrator_module.k8s-cluster-credentials.id
-  resource_id = module.k8s-common.cluster_credentials_resource_id
+  for_each    = toset(var.runtime == "kubernetes" ? ["this"] : [])
+  module_id   = platform-orchestrator_module.k8s-cluster-credentials["this"].id
+  resource_id = module.k8s-common["this"].cluster_credentials_resource_id
 }
