71c14270d041a50bff353e6997406611592adf7e
Ben Meier <ben.meier@humanitec.com>
2025-10-31 11:56:54 +0000

    feat: deploy score workload after infra

--- modules/infra/envs.tf
+++ modules/infra/envs.tf
@@ -8,3 +8,38 @@ resource "platform-orchestrator_environment" "development" {
   env_type_id = platform-orchestrator_environment_type.development.id
   depends_on  = [platform-orchestrator_runner_rule.default]
 }
+
+resource "platform-orchestrator_deployment" "deploy" {
+  project_id = platform-orchestrator_project.project.id
+  env_id     = platform-orchestrator_environment.development.id
+  manifest = jsonencode({
+    workloads = {
+      my-sample-app = {
+        resources = {
+          score-workload = {
+            type = "score-workload"
+            params = {
+              metadata = {
+                name = "my-sample-app"
+              }
+              containers = {
+                main = {
+                  image = "example-app"
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  })
+
+  wait_for = false
+
+  depends_on = [
+    module.aws-infra,
+    module.gcp-infra,
+    module.azure-infra,
+    platform-orchestrator_runner_rule.default,
+  ]
+}
